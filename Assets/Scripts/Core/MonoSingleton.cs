7b591c8829a9d756c4c272a143ae17ec136b4eb0
Hayden Carter <s200476@students.aie.edu.au>
2021-04-30 14:35:55 +1000

    New platformer

---
+++ Assets/Scripts/Core/MonoSingleton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+    protected virtual void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this); //Or GameObject as appropriate
+            return;
+        }
+        Instance = this.gameObject.GetComponent<T>();
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/Custom/Universe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Universe : MonoBehaviour
+{
+    private static Universe instance;
+    public static Universe Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // VARIABLES
+    public bool IsOnWall = false;
+    public int MaxStamina = 8;
+    public int Stamina = 8;
+}
+
