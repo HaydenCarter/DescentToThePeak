c56db0434e4cef8c73141a8c959f620f294e3dcf
Hayden <hcarter015@gmail;.com>
2021-05-14 09:54:19 +1000

    Added Camera Lock Script

---
+++ Assets/Scripts/Custom/LockCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCamera : MonoBehaviour
+{
+    public float xclamp1 = 0f;
+    public float xclamp2 = 1f;
+    public float yclamp1 = 0f;
+    public float yclamp2 = 1f;
+    [SerializeField] private Transform targetToFollow;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(targetToFollow.position.x, xclamp1, xclamp2),
+            Mathf.Clamp(targetToFollow.position.y, yclamp1, yclamp2),
+            transform.position.z);
+    }
+}
