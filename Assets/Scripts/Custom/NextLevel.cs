fe1f61639efd21a10b80cad5cbb2aec112f2ee49
Hayden <hcarter015@gmail;.com>
2021-05-13 10:29:04 +1000

    Added Next Level Script, Fixed Level 2 and Adjusted Climbing Speed

---
+++ Assets/Scripts/Custom/NextLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider collidedObject)
+    {
+        if (collidedObject.gameObject == player)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
