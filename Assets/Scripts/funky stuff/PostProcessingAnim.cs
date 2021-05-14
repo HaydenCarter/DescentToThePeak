7fb95f050401028758b04bbcb21f790e22cd0384
Aaron <A.fuser04@gmail.com>
2021-05-14 15:28:12 +1000

    Post processing added

---
+++ Assets/Scripts/funky stuff/PostProcessingAnim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEditor.PackageManager;
+
+public class PostProcessingAnim : MonoBehaviour
+{
+    public PostProcessVolume _postProcessingVolumeHolder;
+    public float _min, _max;
+
+    public float _chromaValue = 1;
+  
+
+    [Header("EFFECTS")]
+    private ChromaticAberration _ca;
+
+    private void Start()
+    {
+        _postProcessingVolumeHolder.profile.TryGetSettings(out _ca);
+        _ca.intensity.value = _min;
+    }
+
+    FloatParameter _fp = new FloatParameter();
+    // Update is called once per frame
+    void Update()
+    {
+        //if (!isPaused)
+            _ca.intensity.value = _chromaValue;
+    }
+}
