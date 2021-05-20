e844f9d2791861d1439b2ad1cf19c0c5d4b2fd5d
Aaron <A.fuser04@gmail.com>
2021-05-20 12:03:01 +1000

    tilemap update, postprocessing template

--- Assets/Scripts/funky stuff/PostProcessingAnim.cs
+++ Assets/Scripts/funky stuff/PostProcessingAnim.cs
@@ -7,25 +7,39 @@ using UnityEditor.PackageManager;
 public class PostProcessingAnim : MonoBehaviour
 {
     public PostProcessVolume _postProcessingVolumeHolder;
-    public float _min, _max;
+    public float _ca_min, _ca_max, _cs_min, _cs_max;
 
-    public float _chromaValue = 1;
+    public float _chromaValue;
+    public float _hueShiftValue;
+    public float _tintValue;
   
 
     [Header("EFFECTS")]
-    private ChromaticAberration _ca;
+    public ChromaticAberration _ca;
+    public ColorGrading _colourShift;
 
     private void Start()
     {
+
         _postProcessingVolumeHolder.profile.TryGetSettings(out _ca);
-        _ca.intensity.value = _min;
+        _postProcessingVolumeHolder.profile.TryGetSettings(out _colourShift);
+        _chromaValue = _ca.intensity.value;
+       // _hueShiftValue = _colourShift.hueShift.value;
+
+        
+        
     }
 
-    FloatParameter _fp = new FloatParameter();
     // Update is called once per frame
     void Update()
     {
         //if (!isPaused)
-            _ca.intensity.value = _chromaValue;
+        //_ca.intensity.value = _chromaValue;
+
+        _colourShift.hueShift.value = _hueShiftValue;
+
+        _colourShift.tint.value = _tintValue;
+
+        _colourShift.temperature.value = _tintValue * -1;
     }
 }
